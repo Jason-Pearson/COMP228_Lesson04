c41ff4511e53ec6e9dafc14089f244ece3bc7ad8
Jason-Pearson <jason-pearson-1-0-7@hotmail.com>
2015-09-29 12:15:46 -0400

    Added Age And User Input

--- src/Program.java
+++ src/Program.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		int myAge;
+		
+		System.out.print("Please enter your age: ");
+		myAge = input.nextInt();
+		
+		System.out.println("\nYou entered: " + myAge); //+ (variable name) concatenates it to a string - use printf instead to keep the variable type intact to display via format
+		//Good Practice - nest input in a try/catch - to notify user of any mistakes and start over
 	}
 
 }
