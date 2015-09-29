3e22e8fde60604f02746d6c1d38e56e04cf270d9
Jason-Pearson <jason-pearson-1-0-7@hotmail.com>
2015-09-29 12:30:08 -0400

    Added Exception and While Loop

--- src/Program.java
+++ src/Program.java
@@ -3,14 +3,26 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		int myAge;
+		int myAge;// default is 0
+		boolean isValid = false; //check for positive for exception
 		
-		System.out.print("Please enter your age: ");
-		myAge = input.nextInt();
-		
-		System.out.println("\nYou entered: " + myAge); //+ (variable name) concatenates it to a string - use printf instead to keep the variable type intact to display via format
-		//Good Practice - nest input in a try/catch - to notify user of any mistakes and start over
+		//Good Practice - nest input in a try/catch block - to notify user of any mistakes and start over
+		while(!isValid)
+		{
+		try
+		{
+			Scanner input = new Scanner(System.in); //redeclares/re-initializes scanner input per loop - stops infinite loop
+			System.out.print("Please enter your age: ");
+			myAge = input.nextInt();
+			
+			System.out.println("\nYou entered: " + myAge); // (variable name) concatenates it to a string - use printf instead to keep the variable type intact to display via format
+			isValid = true;
+		}
+		catch(Exception exception)//automatically checks nextInt - throws exception when variable input is not integer
+		{
+			System.out.println("Please enter positive number");//stops the try block upon exception error (not displaying age and not making isValid true - hence while loop continues until user types an int)
+		}
+		}
 	}
 
 }
